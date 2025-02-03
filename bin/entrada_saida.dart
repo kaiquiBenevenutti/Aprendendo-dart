3d08bb0c80766fc8e87b9737fd35c7e992fdabce
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2025-02-03 20:54:58 -0300

    entrada e saida de dados

--- bin/entrada_saida.dart
+++ bin/entrada_saida.dart
@@ -1,3 +1,11 @@
-﻿void main(List<String> arguments) {
+﻿import 'dart:io';
 
+void main(List<String> arguments) {
+  String resposta = '';
+
+  while(resposta != 'com quem tu andas'){
+    print('Diga-me com quem tu andas e te direi quem tu és.');
+    resposta = stdin.readLineSync().toString();
+  }
+  print('quem tu és.');
 }
