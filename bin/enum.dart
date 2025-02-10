9db9eb5585bf8ac65911b4f395e69b11d5298673
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2025-02-10 15:48:29 -0300

    Enum

---
+++ bin/enum.dart
@@ -0,0 +1,23 @@
+﻿void main(List<String> arguments){
+  escolherMeioTransporte(Transportes.trem);
+}
+
+escolherMeioTransporte(Transportes locomocao){
+  if(locomocao == Transportes.carro){
+    print("Vou de carro.");
+  }else if(locomocao == Transportes.bike){
+    print("Vou de bike.");
+  }else {
+    print("Nao sei como vou.");
+  }
+}
+
+enum Transportes{
+  carro,
+  bike,
+  andando,
+  skate,
+  aviao,
+  patins,
+  trem
+}
